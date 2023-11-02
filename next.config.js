ae516ef2f40da23fb15d6e07d6ffef6d3c5fb451
Mathias Dahl Jensen <mathias.j@forteadvice.com>
2023-11-02 13:16:30 +0100

    day 1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+      },
+    ],
+  },
+};
+module.exports = nextConfig;
--- src/app/henry/page.jsx
+++ src/app/henry/page.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export async function generateMetadata() {
   const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
   const data = await res.json();
@@ -11,9 +13,18 @@ export default async function Henry() {
   const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
   const data = await res.json();
   const { name, favouriteColor, age, image } = data;
+  console.log(image);
   return (
     <main>
       <h1>{name}</h1>
+      <Image
+        src={image.url}
+        width={image.width}
+        height={image.height}
+        alt={name}
+        priority
+        sizes="(max-width: 500px) 100vw, 500px"
+      />
     </main>
   );
 }
